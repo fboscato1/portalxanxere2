d78da84adefed2a3a16f3f2f89fed364b059e357
fboscato <fboscato@gmail.com>
2015-06-30 22:24:58 -0300

    asdad

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -63,23 +63,21 @@ include the remember me checkbox
 		<div class="container_wrap">
 			<div class="card card-container">
 				<!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
-				<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+				
+				<img id="profile-img" class="img-circle" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
 				<p id="profile-name" class="profile-name-card"></p>
 				<form method="post" class="form-signin" action="index.php" name="loginform">
-					
-					<label for="login_input_username">Username</label>
-					<input id="login_input_username" class="form-control" type="text" name="user_name" required />
-					<input type="login_input_username" class="login_input" placeholder="Usuario" required autofocus>
+
+					<input type="email" id="inputEmail" class="form-control" placeholder="Endereco Email" name="user_name" placeholder="Nome de usuario" required autofocus>
+					<input type="password" id="login_input_username" class="form-control" placeholder="Senha" required name="user_password">
 					<span id="reauth-pass" class="reauth" type></span>
 					<span id="reauth-email" class="reauth-email"></span>
-					<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-					<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
 					<div id="remember" class="checkbox">
 						<label>
 							<input type="checkbox" value="remember-me">
 							Remember me </label>
 					</div>
-					<input  class="btn btn-lg btn-primary btn-block btn-signin" type="submit"  name="login" value="Log in" />					
+					<input  class="btn btn-lg btn-primary btn-block btn-signin" type="submit"  name="login" value="Log in" />
 				</form><!-- /form -->
 				<a href="#" class="forgot-password"> Forgot the password? </a>
 			</div><!-- /card-container -->
